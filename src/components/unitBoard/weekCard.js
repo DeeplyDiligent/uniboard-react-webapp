b4f0c7dfd5a3cef9636062e3a836d8859f1ef4ae
Deep Bhattacharyya <bhattacharyya.deep@gmail.com>
2019-01-04 10:19:24 +1100

    fixed windows compilation

--- src/components/unitBoard/weekCard.js
+++ src/components/unitBoard/weekCard.js
@@ -3,7 +3,7 @@ class WeekCard extends Component {
     render() { 
         return ( 
             <div className="py-2 max-w-md w-full lg:flex">
-                <div className="min-w-full sm:invisible border-r border-b border-l border-grey-light  lg:border-t lg:border-grey-light bg-white rounded-b lg:rounded-b-none lg:rounded-r p-4 flex flex-col justify-between leading-normal">
+                <div className="min-w-full border-r border-b lgborder-l border-grey-light lg:border-t lg:border-grey-light bg-white rounded-b lg:rounded-b-none lg:rounded-r p-4 flex flex-col justify-between leading-normal">
                     <div className="text-black font-bold text-xl">Week X</div>
                     <p className="py-5 text-grey-darker text-base">Week Preview...</p>
                     <div style={{width:"fit-content",marginLeft:"auto",marginRight:"0px"}} className="content-end">
