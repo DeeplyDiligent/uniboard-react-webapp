1aa1c2c8d5d753427eacd75da5562f94f7a4edd2
Deep Bhattacharyya <bhattacharyya.deep@gmail.com>
2019-01-04 11:56:44 +1100

    sidebar added

---
+++ src/components/sidebar/sidebar.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+class Sidebar extends Component {
+    state = {  }
+
+    render() {
+        const styles = {}
+        styles.sidebar = {right:0, width:"33%",height:"100%"}
+        return (
+            <div className="absolute bg-white max-w-sm rounded overflow-hidden shadow-lg px-4 py-4" style={styles.sidebar}>
+                <div className="text">{this.props.match.params.id}</div>
+                <div className="text-black">UnitName</div>
+            </div>
+        );
+    }
+}
+ 
+export default Sidebar;
