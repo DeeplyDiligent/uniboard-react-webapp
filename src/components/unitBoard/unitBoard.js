b5b42a9be9e42ea012fd5f127f86f3ac6b0d3039
sameersyedhd <sameersyedhd@users.noreply.github.com>
2019-01-04 11:23:42 +1100

    named unit appropriately

--- src/components/unitBoard/unitBoard.js
+++ src/components/unitBoard/unitBoard.js
@@ -4,7 +4,7 @@ class UnitBoard extends Component {
     render() { 
         return ( <div style={{minWidth:"250px"}} className="flex-1 max-w-sm rounded overflow-hidden shadow-lg m-4">
         <div className="px-6 py-4 bg-green-lightest">
-          <div className="font-bold text-4xl mb-2">BFC3241</div>
+          <div className="font-bold text-4xl mb-2">KFC3241</div>
         </div>
         <div className="px-4 py-4">
             {Array(10).fill(1).map((i,j)=><WeekCard key={j} />)}
