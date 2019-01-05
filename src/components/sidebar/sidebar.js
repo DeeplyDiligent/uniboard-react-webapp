55e0e0a1044615868ec33f975872f7587192dded
sameersyedhd <sameersyedhd@users.noreply.github.com>
2019-01-05 13:20:34 +1100

    fixed position sidebar

--- src/components/sidebar/sidebar.js
+++ src/components/sidebar/sidebar.js
@@ -9,7 +9,7 @@ class Sidebar extends Component {
         const styles = {}
         styles.sidebar = {right:0, width:"40%",height:"100%"}
         return (
-            <div className="absolute bg-white max-w-sm rounded overflow-hidden shadow-lg px-8 py-4" style={styles.sidebar}>
+            <div className="fixed bg-white max-w-sm rounded overflow-hidden shadow-lg px-8 py-4" style={styles.sidebar}>
                 <div className="text-right pb-2 text-3xl"><i className="fas fa-times"></i></div>
                 <div className="text-2xl font-bold">Unit Name</div>
                 {/* <div className="text">{this.props.match.params.id}</div> */}
