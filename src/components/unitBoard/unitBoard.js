df90d8e7bfb4fd0361ac9d985914398c10c2fe23
Deep Bhattacharyya <bhattacharyya.deep@gmail.com>
2019-01-11 14:14:45 +1100

    scroll bar fixes

--- src/components/unitBoard/unitBoard.js
+++ src/components/unitBoard/unitBoard.js
@@ -2,14 +2,14 @@ import React, { Component } from 'react';
 import WeekCard from './weekCard';
 class UnitBoard extends Component {
     render() { 
-        return ( <div style={{minWidth:"250px", height: 'calc(100% - 32px)', overflow:'scroll'}} className="flex-1 max-w-sm rounded overflow-hidden shadow-lg m-2 bg-white">
-        <div className="px-6 py-4 bg-white " >
+        return ( <div style={{minWidth:"250px", height: 'calc(100% - 32px)', overflow:'hidden'}} className="flex-1 max-w-sm rounded overflow-hidden shadow-lg m-2 bg-white">
+        <div className="px-6 py-4 bg-white" >
         <div className='flex'> 
         <i className="fas fa-graduation-cap mt-2"></i>
           <div className=" text-3xl ml-4">KFC3241</div>
           </div>
         </div>
-        <div className="px-4 py-4" style={ {overflow: 'scroll', height: 'calc(100% - 66px)'}}>
+        <div className="px-4 py-4" style={ {overflowY: 'scroll', height: 'calc(100% - 66px)'}}>
             {Array(10).fill(1).map((i,j)=><WeekCard key={j} />)}
         </div>
       </div> );
