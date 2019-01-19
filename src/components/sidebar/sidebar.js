9ea08e6ea66719aab3628673f53689be8025f965
Haikal Zain <haikalzain2010@gmail.com>
2019-01-19 18:16:28 +1100

    add state to sidebar

--- src/components/sidebar/sidebar.js
+++ src/components/sidebar/sidebar.js
@@ -10,45 +10,55 @@ const Box = posed.div({
     })
 
 class Sidebar extends Component {
-    state = { isVisible : false}
+    state = { isVisible : false, data: null}
 
     componentDidMount(){
         this.setState({isVisible:true})
-    }
-    render() {
-        
-        // console.log(this.db.collection('dba').doc('eGlJZRO3Bv0h5OUOENIa'))
-        this.props.db.get().then(function(doc) {
+        this.props.db.get().then((doc)=> {
             if (doc.exists) {
+                this.setState({data: doc.data()});
                 console.log("Document data:", doc.data());
             }
-        });        
+        });
+    }
 
+    render() {
+        
+        // console.log(this.db.collection('dba').doc('eGlJZRO3B
+        console.log(this.props)
         const styles = {}   
         styles.sidebar = {right:0, zIndex:'1000', overflow:'scroll'}
         styles.sidebarBackground = {rgba:'(0,0,0,0)'}
-        return (
-            
-            <div className="w-full h-full absolute" >
-               <Link to='/home'> 
-                <div className="sidebarBackground h-full" style={styles.sidebarBackground}></div>
-               </Link>
-               <Box pose={this.state.isVisible ? 'visible' : 'hidden'} className="sidebarContent fixed bg-white max-w-sm rounded h-full shadow-lg px-8 py-4 sm:w-full lg:w-1/3 h-full" style={styles.sidebar}>                    
-                    <Link to='/home'>
-                    <div className="text-right pb-2 text-3xl">
-                    <i className="fas fa-times"></i></div>
-                    </Link>
-                    <div className="text-2xl font-bold">Unit Name</div>
-                    {/* <div className="text">{this.props.match.params.id}</div> */}
-                    <div className="bg-red-light brow my-4"></div>
-                    <div className = "text-md text-grey">Week name and topic (if applicable)</div>
-                    <Attachments />
-                    <hr/>
-                    <TodoApp/>
-                </Box>
-            </div>
-            
-        );
+        if(this.state.data){
+            return (
+                <div className="w-full h-full absolute" >
+                <Link to='/home'> 
+                    <div className="sidebarBackground h-full" style={styles.sidebarBackground}></div>
+                </Link>
+                <Box pose={this.state.isVisible ? 'visible' : 'hidden'} className="sidebarContent fixed bg-white max-w-sm rounded h-full shadow-lg px-8 py-4 sm:w-full lg:w-1/3 h-full" style={styles.sidebar}>                    
+                        <Link to='/home'>
+                        <div className="text-right pb-2 text-3xl">
+                        <i className="fas fa-times"></i></div>
+                        </Link>
+                        <div className="text-2xl font-bold">{this.props.match.params.id}</div>
+                        {/* <div className="text">{this.props.match.params.id}</div> */}
+                        <div className="bg-red-light brow my-4"></div>
+                        <div className = "text-md text-grey">Week name and topic (if applicable)</div>
+                        <Attachments />
+                        <hr/>
+                        <TodoApp/>
+                    </Box>
+                </div>
+                
+            );
+        }
+        else {
+            return(
+                <div>
+                    Waiting for data...
+                </div>
+            )
+        }
     }
 }
  
