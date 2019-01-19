e83aa4636adfbc616e94fb91e72c6359d7c2dd75
Deep Bhattacharyya <bhattacharyya.deep@gmail.com>
2019-01-19 18:12:54 +1100

    code beautification

--- src/components/home.js
+++ src/components/home.js
@@ -1,7 +1,7 @@
-import React, { Component } from 'react';
-import UnitBoards from './unitBoard/unitBoards'
-import Search from './sidebar/search'
-import firebase from 'firebase';
+import React, { Component } from "react";
+import UnitBoards from "./unitBoard/unitBoards";
+import Search from "./sidebar/search";
+import firebase from "firebase";
 import {
   BrowserView,
   MobileView,
@@ -9,51 +9,45 @@ import {
   isMobileOnly
 } from "react-device-detect";
 
-
 class Home extends Component {
-
-  state = { 
-    data : '',
-  }
-  constructor(props){
-    super(props)
+  state = {
+    data: ""
+  };
+  constructor(props) {
+    super(props);
     var context = this;
     this.props.db.get().then(function(doc) {
       if (doc.exists) {
-          context.setState({data : doc.data()})
+        context.setState({ data: doc.data() });
       }
     });
   }
 
-  componentDidUpdate(){
-    var displayString = isMobileOnly ? 'none' : 'table'
-    return (displayString)
+  componentDidUpdate() {
+    var displayString = isMobileOnly ? "none" : "table";
+    return displayString;
   }
 
-
   render() {
-    if (this.state.data){
+    if (this.state.data) {
       // console.log(this.state.data);
       return (
-        <div className='h-full bg-grey-lightest '>
-          <div className='searchBox	m-auto pt-4' style={{display: this.componentDidUpdate()}} >
-          <Search/>
-          <div/>
-          <div/>
+        <div className="h-full bg-grey-lightest ">
+          <div
+            className="searchBox	m-auto pt-4"
+            style={{ display: this.componentDidUpdate() }}
+          >
+            <Search />
+            <div />
+            <div />
+          </div>
+          <UnitBoards database={this.state.data} />
         </div>
-        <UnitBoards database={this.state.data}/>
-        </div>
-        
       );
+    } else {
+      return <div>Begging Moodle for Data...</div>;
     }
-    else{
-      return(
-      <div>
-        Begging Moodle for Data...
-      </div>
-      )
-    }
-    }
+  }
 }
 
 export default Home;
