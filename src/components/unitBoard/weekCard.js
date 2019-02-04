728ff4235dfbe1a89eaea6a36cc864576baceb35
Deep Bhattacharyya <bhattacharyya.deep@gmail.com>
2019-02-04 11:58:54 +1100

    weekcard shows number of files

--- src/components/unitBoard/weekCard.js
+++ src/components/unitBoard/weekCard.js
@@ -1,35 +1,40 @@
-import React, { Component } from 'react';
-import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
+import React, { Component } from "react";
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+import database from '../../data';
 
 class WeekCard extends Component {
-
-constructor(){
-    super()
-    this.handleClick = this.handleClick.bind(this)
+  render() {
+    return (
+      <Link
+        to={"/home/sidebar/" + this.props.unitName + "/" + this.props.branchId}
+        style={{ textDecoration: "none" }}
+      >
+        <div
+          style={{ width: "100%" }}
+          className="border-2 rounded border-light-grey p-4 my-2"
+        >
+          <div>
+            <div className="brow bg-red-light mb-1" />
+            <div className="text-grey-darkest font-medium text-lg">
+              {this.props.weekName}
+            </div>
+          </div>
+          <div
+            style={{
+              width: "fit-content",
+              marginLeft: "auto",
+              marginRight: "0px"
+            }}
+            className="self-end mt-2"
+          >
+            <span className="px-3 py-1 text-sm font-semibold text-grey">
+              Files: {database.getAllAttachments(this.props.data).length}
+            </span>
+          </div>
+        </div>
+      </Link>
+    );
+  }
 }
 
-    handleClick(){
-    
-//this.context.history.push('/home/sidebar/kfc3241')
- }
-
-
-    render() { 
-         return (
-             <Link to={'/home/sidebar/'+this.props.unitName+'/'+this.props.branchId} style={{textDecoration:'none'}}>
-             <div style={{width: "100%"}} className="border-2 rounded border-light-grey p-4 my-2">
-                 <div>
-                    <div className="brow bg-red-light mb-1"></div>
-                    <div className="text-grey-darkest font-medium text-lg">{this.props.weekName}</div>
-                 </div>
-                 <div style={{width:"fit-content",marginLeft:"auto",marginRight:"0px"}} className="self-end mt-2">
-                    <span className="px-3 py-1 text-sm font-semibold text-grey">Tasks: 6</span>
-                    <span className="px-3 py-1 text-sm font-semibold text-grey">4/5</span>
-                </div>
-             </div>
-             </Link>
-         )
-    }
-}
- 
 export default WeekCard;
